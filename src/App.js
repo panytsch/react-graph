8eef0030146a78244c0e87a178f0e0485f88b289
panytsch <romeo.crary@gmail.com>
2018-05-17 01:22:45 +0300

    just add stylesheets

--- src/App.js
+++ src/App.js
@@ -2,20 +2,21 @@ import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import SocketLine from "./graph/SocketLine";
+import History from "./graph/History";
 import Price from "./currency/Price";
 import Calculator from "./currency/Calculator";
 
 class App extends Component {
-  render() {
-    return (
-      <div>
-        {/* <Price /> */}
-        {/* <Calculator /> */}
-
-        {/* <SocketLine /> */}
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div>
+				<Price />
+				<Calculator />
+				<History />
+				<SocketLine />
+			</div>
+		);
+	}
 }
 
 export default App;
